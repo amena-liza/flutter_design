22334277338fd8637a1ac88d112024982b1bb619
Amena <amenakhatunliza91@gmail.com>
2026-05-31 12:11:38 +0600

    add: Image Layout Practise

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.light,
         ),
       ),
-      home: const ColumnLayoutPractise(title: 'Hello',),
+      home: const ImageLayoutPractise(title: 'Hello Image Layout',),
     );
   }
 }
@@ -141,6 +141,32 @@ class ColumnLayoutPractise extends StatelessWidget {
             ),
           ),
     )
+    );
+  }
+}
+
+class ImageLayoutPractise extends StatelessWidget {
+  const ImageLayoutPractise({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(title),
+        ),
+        body: Image.asset(
+          'assets/images/bg.jpg',
+          height: double.infinity,
+          fit: BoxFit.cover,
+        )
     );
   }
 }
